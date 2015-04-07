00c7d464049500865968f5dffc6e6420a52c7e68
Peter Corazao <pcorazao@nc2media.com>
2015-04-07 15:34:27 -0500

    init
    working

---
+++ AssetBundles/Assets/editor/CreateAssetBundles.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+
+public class CreateAssetBundles{
+
+	[MenuItem ("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles ()
+	{
+		BuildPipeline.BuildAssetBundles ("AssetBundles");
+	}
+
+	[MenuItem("Assets/Create From Script")]
+	public static void CreateNewAssetBundleFromScript()
+	{
+		string outputPath = "AssetBundlesFromScript";
+		
+		AssetImporter cubeImporter = AssetImporter.GetAtPath("Assets/prefab/Cube.prefab");
+		cubeImporter.assetBundleName = "cube.unity3d";
+
+
+		AssetImporter materialImporter = AssetImporter.GetAtPath("Assets/material/MyMaterial.mat");
+		materialImporter.assetBundleName = "material.unity3d";
+
+		AssetImporter game1Importer = AssetImporter.GetAtPath("Assets/images/UnityLogo.png");
+		game1Importer.assetBundleName = "texture.unity3d";
+
+		BuildPipeline.BuildAssetBundles(outputPath);
+	}
+
+	[MenuItem("Assets/Create From AssetBundleBuild")]
+	public static void CreateNewAssetBundleFromAssetBundleBuild()
+	{
+		string outputPath = "AssetBundlesFromBuildInfo";
+		
+		AssetBundleBuild[] buildInfo = new []
+		{
+			new AssetBundleBuild{ assetBundleName = "cube.unity3d", assetNames = new [] {"Assets/prefab/Cube.prefab"}},
+			new AssetBundleBuild{ assetBundleName = "material.unity3d", assetNames = new [] {"Assets/material/MyMaterial.mat"}},
+			new AssetBundleBuild{ assetBundleName = "texture.unity3d", assetNames = new [] {"Assets/images/UnityLogo.png"}}
+		};
+		
+		BuildPipeline.BuildAssetBundles(outputPath,buildInfo);
+	}
+}
---
+++ AssetBundles/Assets/scripts/LoadAssetBundles.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadAssetBundles : MonoBehaviour {
+
+	public string assetBundleFolder = "file:///Users/pcorazao/Documents/Unity/AssetBundlesRound2/AssetBundles/";
+	public string cubeAssetBundle = "cube.unity3d";
+	
+	IEnumerator Start()
+	{
+		string manifestABPath = assetBundleFolder + "AssetBundles";
+		
+		var wwwManifest = new WWW (manifestABPath);
+		yield return wwwManifest;
+		
+		AssetBundle manifestBundle = wwwManifest.assetBundle;
+		
+		AssetBundleManifest manifest = manifestBundle.LoadAsset ("AssetBundleManifest") as AssetBundleManifest;
+		manifestBundle.Unload (false);
+		
+		//get dependencies
+		string[] dependentAssetBundles = manifest.GetAllDependencies (cubeAssetBundle);
+		for (int i = 0; i < dependentAssetBundles.Length; i++) {
+			Debug.Log(dependentAssetBundles[i]);
+		}
+		
+		//load dependent assetbundles
+		AssetBundle[] assetBundles = new AssetBundle[dependentAssetBundles.Length];
+		for (int i = 0; i < dependentAssetBundles.Length; i++) {
+			string assetBundlePath = assetBundleFolder + dependentAssetBundles[i];
+			
+			//Get the hash
+			Hash128 hash = manifest.GetAssetBundleHash(dependentAssetBundles[i]);
+			var www = WWW.LoadFromCacheOrDownload(assetBundlePath, hash);
+			yield return www;
+			assetBundles[i] = www.assetBundle;
+		}
+		
+		//load game1 assetBundle
+		var wwwCube = WWW.LoadFromCacheOrDownload (assetBundleFolder + cubeAssetBundle, manifest.GetAssetBundleHash (cubeAssetBundle));
+		yield return wwwCube;
+		AssetBundle cubeBundle = wwwCube.assetBundle;
+		
+		// Load game1
+		GameObject cubePrefab = (GameObject)cubeBundle.LoadAsset("Cube");
+		GameObject.Instantiate(cubePrefab);
+		
+		for (int i = 0; i < dependentAssetBundles.Length; i++) {
+			//assetBundles[i].Unload(false);
+		}
+		
+		//Unload cube assetBundle
+		cubeBundle.Unload(false);
+	}
+}
